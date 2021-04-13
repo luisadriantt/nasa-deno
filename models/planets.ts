4f71edb3586893a13a4a5427e9026d9698918e67
ltobar <luisadrianta1@gmail.com>
2021-04-12 20:15:21 -0500

    ADD: testing

--- models/planets.ts
+++ models/planets.ts
@@ -6,48 +6,61 @@ import * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
 
 type Planet = Record<string, string>;
 
-let planets: Array<Planet>;
+export const filterHabitablePlanets = (planets: Array<Planet>) => {
+  return (
+    planets &&
+    planets.filter((planet) => {
+      const planetaryRadius = Number(planet["koi_prad"]);
+      const stellarMass = Number(planet["koi_smass"]);
+      const stellarRadius = Number(planet["koi_srad"]);
+
+      return (
+        planet["koi_disposition"] === "CONFIRMED" &&
+        planetaryRadius > 0.5 &&
+        planetaryRadius < 1.5 &&
+        stellarMass > 0.78 &&
+        stellarMass < 1.04 &&
+        stellarRadius > 0.99 &&
+        stellarRadius < 1.01
+      );
+    })
+  );
+};
 
 const loadPlanetsData = async () => {
   const path = join("data", "kepler_exoplanets_nasa.csv");
 
-  const file = await Deno.open(path);
-  const bufReader = new BufReader(file);
-  const result = await parse(bufReader, { comment: "#", skipFirstRow: true });
+  let result;
 
-  Deno.close(file.rid);
+  try {
+    const file = await Deno.open(path);
+    const bufReader = new BufReader(file);
+    result = await parse(bufReader, { comment: "#", skipFirstRow: true });
 
-  const planets = (result as Array<Planet>).filter((planet) => {
-    const planetaryRadius = Number(planet["koi_prad"]);
-    const stellarMass = Number(planet["koi_smass"]);
-    const stellarRadius = Number(planet["koi_srad"]);
+    Deno.close(file.rid);
+  } catch (error) {
+    console.log(error);
+  }
+  const planets = filterHabitablePlanets(result as Array<Planet>);
 
-    return (
-      planet["koi_disposition"] === "CONFIRMED" &&
-      planetaryRadius > 0.5 &&
-      planetaryRadius < 1.5 &&
-      stellarMass > 0.78 &&
-      stellarMass < 1.04 &&
-      stellarRadius > 0.99 &&
-      stellarRadius < 1.01
-    );
-  });
-
-  return planets.map((planet) => {
-    return _.pick(planet, [
-      "kepler_name",
-      "koi_prad",
-      "koi_smass",
-      "koi_srad",
-      "koi_count",
-      "koi_steff",
-      "koi_period",
-    ]);
-  });
+  if (planets) {
+    return planets.map((planet) => {
+      return _.pick(planet, [
+        "kepler_name",
+        "koi_prad",
+        "koi_smass",
+        "koi_srad",
+        "koi_count",
+        "koi_steff",
+        "koi_period",
+      ]);
+    });
+  }
 };
 
-planets = await loadPlanetsData();
-console.log(`${planets.length} habitable planets found`);
+const planets = await loadPlanetsData();
+const amount = planets?.length || 0;
+console.log(`${amount} habitable planets found`);
 
 export const getAllPlanets = () => {
   return planets;
